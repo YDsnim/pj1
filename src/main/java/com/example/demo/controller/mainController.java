08cbe6f63bb6a9bd1ca712a82d98d5a9b9826317
YD <dydeka1111@gmail.com>
2024-01-25 14:33:08 +0900

    .save = .update
    일대다 다대일 이해 못함
    product만 ManytoOne 설정
    
    임시 생산계획을 작성하는 html 임시 생성
    맵핑 해놓음
    
    교과서 100p
    다음 할것
    
    1. DB로부터 리스트를 뷰에 출력 (html) 이용해서
    2. 뷰 로부터 DB에 추가
    3. 조회/삭제/수정/등록
    4. 일대다 다대일?
    5. ajax구현?

--- src/main/java/com/example/demo/controller/mainController.java
+++ src/main/java/com/example/demo/controller/mainController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.SampleDTO;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -49,4 +54,20 @@ public class mainController {
         log.info("ex3 진입");
     }
 
+    @GetMapping({"/ProductionPlan","exLink"})
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1,20)
+                //asLongStream : ㅣIntStream의 int요소를 Long으로 변환해줌
+                .asLongStream()
+                .mapToObj(i->{
+                    SampleDTO dto = SampleDTO.builder()
+                            .ProductionPK("PK100")
+                            .ProductionDate(Date.from(Instant.now()))
+                            .ProductionQuantity(100L)
+                            .complete(false)
+                            .build();
+                    return dto;
+                }).collect(Collectors.toList());
+        model.addAttribute("list",list);
+    }
 }
